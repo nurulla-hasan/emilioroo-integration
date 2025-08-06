409fe6fe927738f4167468ddfa0288b42c94f05e
nurulla-hasan <nurullahasan.dev@gmail.com>
2025-08-06 10:03:15 +0600

    first commit

---
+++ src/app/(main-route)/page.js
@@ -0,0 +1,12 @@
+import FeaturedProducts from "@/components/home/FeaturedProducts";
+import Hero from "@/components/home/Hero";
+
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <FeaturedProducts />
+    </>
+  );
+}
