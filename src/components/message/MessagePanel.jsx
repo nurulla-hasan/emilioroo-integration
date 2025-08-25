b4de4a979ff18860695ce28a64568832861b5d28
nurulla-hasan <nurullahasan.dev@gmail.com>
2025-08-25 17:52:19 +0600

    update message

--- src/components/message/MessagePanel.jsx
+++ src/components/message/MessagePanel.jsx
@@ -4,9 +4,10 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { Send, Smile, PlusCircle, Image as ImageIcon, Info, ArrowLeft } from 'lucide-react';
+import { useEffect, useRef } from 'react';
 
 import { Message } from './Message';
+import { ArrowLeft, ImageUpIcon, Info, PlusCircle, Send, Smile } from "lucide-react";
 
 export const MessagePanel = ({
     conversation,
@@ -17,6 +18,15 @@ export const MessagePanel = ({
     setNewMessage,
     onSendMessage
 }) => {
+    const messagesEndRef = useRef(null);
+
+    const scrollToBottom = () => {
+        messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+    };
+
+    useEffect(() => {
+        scrollToBottom();
+    }, [messages]);
     return (
         <div className="w-full bg-card flex flex-col h-full">
             <div className="p-3 border-b flex items-center justify-between shadow-sm">
@@ -41,15 +51,16 @@ export const MessagePanel = ({
                     </Button>
                 </div>
             </div>
-            <ScrollArea className="flex-1 h-96 p-4 bg-background/50">
+            <ScrollArea className="flex-1 p-4 h-96 bg-background/50">
                 {messages.map(msg => <Message key={msg.id} msg={msg} />)}
+                <div ref={messagesEndRef} />
             </ScrollArea>
             <div className="p-4 border-t bg-card"><div className="relative flex items-center gap-2">
                 <Button variant="ghost" size="icon">
                     <PlusCircle />
                 </Button>
                 <Button variant="ghost" size="icon">
-                    <ImageIcon />
+                    <ImageUpIcon />
                 </Button><div className="flex-1 relative">
                     <Input
                         placeholder="Aa" value={newMessage}
