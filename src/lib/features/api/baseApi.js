ae3e2f27020943bc06a6c7d12626a1f75d69f5fe
Nurulla Hasan <85323050+nurulla-hasan@users.noreply.github.com>
2025-09-27 12:43:38 +0600

    Update baseApi.js

--- src/lib/features/api/baseApi.js
+++ src/lib/features/api/baseApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const baseQuery = fetchBaseQuery({
     // baseUrl: 'http://10.10.20.70:4000',
-    // baseUrl: 'http://10.10.20.9:4000',
+    // baseUrl: 'http://18.231.80.188:5000',
     // baseUrl: 'https://rnj64vmh-4000.inc1.devtunnels.ms',
     baseUrl: 'https://api.bankybondy.com',
     prepareHeaders: (headers, { getState }) => {
