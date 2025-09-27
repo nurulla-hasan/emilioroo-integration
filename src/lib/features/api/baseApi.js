43cb72d0383808ec1bdea3927a43dd536b92a248
manik <maniksarker652@gmail.com>
2025-09-27 11:54:44 +0600

    s

--- src/lib/features/api/baseApi.js
+++ src/lib/features/api/baseApi.js
@@ -1,9 +1,10 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const baseQuery = fetchBaseQuery({
     // baseUrl: 'http://10.10.20.70:4000',
     // baseUrl: 'http://10.10.20.9:4000',
-    baseUrl: 'https://rnj64vmh-4000.inc1.devtunnels.ms',
+    // baseUrl: 'https://rnj64vmh-4000.inc1.devtunnels.ms',
+    baseUrl: 'https://api.bankybondy.com',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.accessToken;
 
